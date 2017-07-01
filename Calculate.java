0a891abd6e6295f7c8bee32e62a03216188f51bb
Kirsanov Dmitry <dv.kirsan0v@yandex.ru>
2017-07-01 23:09:13 +0300

    First commit :)

--- Calculate.java
+++ Calculate.java
@@ -1,7 +1,9 @@
 public class Calculate {
-
 		public static void main(String[] arg) {
-			System.out.println("Hello, World!");
+			System.out.println("Calculate...");
+			int a = Integer.valueOf(arg[0]);
+			int b = Integer.valueOf(arg[1]);
+			int c = a + b;
+			System.out.println("Sum result: " + c);
 		}
 }
-
